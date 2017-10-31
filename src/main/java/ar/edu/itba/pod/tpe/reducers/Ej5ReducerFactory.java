16871a3b95221e4f28a72292a76f111136641563
maggievega <mvega@itba.edu.ar>
2017-10-31 19:36:16 -0300

    Removed unnecesary for that can be replaced with a variable

--- src/main/java/ar/edu/itba/pod/tpe/reducers/Ej5ReducerFactory.java
+++ src/main/java/ar/edu/itba/pod/tpe/reducers/Ej5ReducerFactory.java
@@ -4,7 +4,7 @@ import com.hazelcast.mapreduce.Reducer;
 import com.hazelcast.mapreduce.ReducerFactory;
 
 import java.text.DecimalFormat;
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class Ej5ReducerFactory implements ReducerFactory<String, Integer, Float> {
     @Override
@@ -14,28 +14,24 @@ public class Ej5ReducerFactory implements ReducerFactory<String, Integer, Float>
 
     private class Ej5Reducer extends Reducer<Integer, Float> {
 
-        private HashMap<Integer,Integer> houses;
+        private HashSet<Integer> houses;
+        private int counterPop = 0;
 
         Ej5Reducer(){
-            houses = new HashMap<>();
+            houses = new HashSet<>();
         }
         @Override
         public void reduce(Integer houseHold) {
-            if(houses.containsKey(houseHold)){
-                houses.put(houseHold, houses.get(houseHold) + 1);
-            }else{
-                houses.put(houseHold,1);
+            if(!houses.contains(houseHold)){
+                houses.add(houseHold);
             }
+            counterPop += 1;
         }
 
         @Override
         public Float finalizeReduce() {
-            float sum = 0;
-            for (int i: houses.values()){
-                sum += i;
-            }
             DecimalFormat decimalFormat = new DecimalFormat("#.##");
-            return Float.valueOf(decimalFormat.format(sum / houses.size()));
+            return Float.valueOf(decimalFormat.format(counterPop / houses.size()));
         }
     }
 }
