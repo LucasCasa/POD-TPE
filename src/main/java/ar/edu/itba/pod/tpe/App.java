a4e9485e55167b82ad3308abaee1b4683d21c105
LucasCasa <lcasagrande@itba.edu.ar>
2017-10-31 20:11:54 -0300

    starting with params

--- src/main/java/ar/edu/itba/pod/tpe/App.java
+++ src/main/java/ar/edu/itba/pod/tpe/App.java
@@ -1,19 +1,16 @@
 package ar.edu.itba.pod.tpe;
 
-import ar.edu.itba.pod.tpe.mappers.Ej6Mapper;
 import ar.edu.itba.pod.tpe.mappers.Ej7Mapper;
-import ar.edu.itba.pod.tpe.reducers.Ej6ReducerFactory;
 import ar.edu.itba.pod.tpe.reducers.Ej7ReducerFactory;
 import ar.edu.itba.pod.tpe.submitters.ProvincePairCounterCollator;
-import ar.edu.itba.pod.tpe.submitters.TopNFromDescendantsWithMinimumSortedCollator;
 import ar.edu.itba.pod.tpe.utils.CensusEntry;
 import ar.edu.itba.pod.tpe.utils.KeyValue;
+import ar.edu.itba.pod.tpe.utils.ParamLoader;
 import ar.edu.itba.pod.tpe.utils.ProvincePair;
 import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.config.Config;
 import com.hazelcast.core.*;
 import com.hazelcast.mapreduce.*;
+import org.apache.commons.cli.*;
 
 import java.io.File;
 import java.util.Scanner;
@@ -21,22 +18,32 @@ import java.util.Set;
 
 public class App {
     public static void main(String[] args) throws Exception{
-        HazelcastInstance h = Hazelcast.newHazelcastInstance();
-        final HazelcastInstance hi = HazelcastClient.newHazelcastClient();
+
+    	final HazelcastInstance hi = HazelcastClient.newHazelcastClient();
+		ParamLoader params = new ParamLoader();
 
         JobTracker jt = hi.getJobTracker("province-pairs");
-        Scanner s = new Scanner(new File("census100.csv"));
-        ISet<CensusEntry> set = hi.getSet("censusData669");
+        Scanner s = new Scanner(new File(params.getDataPath()));
+        ISet<CensusEntry> set = hi.getSet("DataSet");
         while(s.hasNextLine()){
             set.add(new CensusEntry(s.nextLine()));
         }
+
         final KeyValueSource<String, CensusEntry> source = KeyValueSource.fromSet(set);
         Job<String, CensusEntry> job = jt.newJob(source);
-
-        ICompletableFuture<Set<KeyValue<ProvincePair,Integer>>> future = job
-                .mapper(new Ej7Mapper())
-                .reducer(new Ej7ReducerFactory())
-                .submit(new ProvincePairCounterCollator(2));
+		ICompletableFuture<Set<KeyValue>> future;
+		if(params.hasCombiner()){
+			future = job
+					.mapper(params.getMapper())
+					.reducer(params.getReducer())
+					.submit(params.getCollator());
+		}else{
+			future = job
+					.mapper(params.getMapper())
+					.combiner(params.getCombiner())
+					.reducer(params.getReducer())
+					.submit(params.getCollator());
+		}
 
         System.out.println(future.get());
     }
---
+++ src/main/java/ar/edu/itba/pod/tpe/utils/ParamLoader.java
@@ -0,0 +1,121 @@
+package ar.edu.itba.pod.tpe.utils;
+
+import ar.edu.itba.pod.tpe.combiner.Ej1CombinerFactory;
+import ar.edu.itba.pod.tpe.mappers.*;
+import ar.edu.itba.pod.tpe.reducers.*;
+import ar.edu.itba.pod.tpe.submitters.*;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.mapreduce.*;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class ParamLoader {
+		private String address, data, outPath, timeOutPath;
+		private int query;
+	public ParamLoader(){
+		address = System.getProperty("address");
+		query = Integer.parseInt(System.getProperty("query"));
+		data = System.getProperty("inPath");
+		outPath = System.getProperty("outPath");
+		timeOutPath = System.getProperty("timeOutPath");
+	}
+
+	public ClientConfig getConfig(){
+		throw new NotImplementedException();
+	}
+
+	public Mapper getMapper(){
+		switch(query){
+			case 1:
+				return new Ej1Mapper();
+			case 2:
+				String prov = System.getProperty("prov");
+				if(prov != null)
+					return new Ej2Mapper(System.getProperty("prov"));
+				else
+					throw new IllegalArgumentException("prov argument missing");
+			case 3:
+				return new Ej3Mapper();
+			case 4:
+				return new Ej4Mapper();
+			case 5:
+				return new Ej5Mapper();
+			case 6:
+				return new Ej6Mapper();
+			case 7:
+				return new Ej7Mapper();
+			default:
+				throw new IllegalArgumentException("Invalid query number");
+
+		}
+	}
+
+	public ReducerFactory getReducer(){
+		switch(query){
+			case 1:
+				return new Ej1ReducerFactory();
+			case 2:
+				return new Ej2ReducerFactory();
+			case 3:
+				return new Ej3ReducerFactory();
+			case 4:
+				return new Ej4ReducerFactory();
+			case 5:
+				return new Ej5ReducerFactory();
+			case 6:
+				return new Ej6ReducerFactory();
+			case 7:
+				return new Ej7ReducerFactory();
+			default:
+				throw new IllegalArgumentException("Invalid query number");
+
+		}
+	}
+
+	public Collator getCollator(){
+		int n = 0;
+		if(query == 2 || query == 6 || query == 7)
+			n = Integer.parseInt(System.getProperty("n"));
+
+		switch(query){
+			case 1:
+				return new DescendantSortedCollator();
+			case 2:
+				return new TopNFromDescendantSortedCollator(n);
+			case 3:
+				return new EmploymentRatioCollator();
+			case 4:
+				return new DescendantSortedCollator();
+			case 5:
+				return new DescendantSortedCollator();
+			case 6:
+				return new DescendantSortedWithMinimumCollator<>(n);
+			case 7:
+				return new ProvincePairCounterCollator(n);
+			default:
+				throw new IllegalArgumentException("Invalid query number");
+
+		}
+	}
+
+	public boolean hasCombiner(){
+		 return query == 1 || query == 2;
+	}
+
+	public CombinerFactory getCombiner(){
+		switch (query){
+			case 1:
+			case 2:
+				return new Ej1CombinerFactory();
+			default:
+				throw new IllegalArgumentException("This query has no combiner");
+		}
+	}
+
+	public void loadLog(){
+
+	}
+
+	public String getDataPath() {
+		return data;
+	}
+}
